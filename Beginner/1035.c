9f68e4f7abea22fcbe9f00b69967a4f6250c398d
Gabriel Oliveira <oliveirasgah@gmail.com>
2016-02-26 17:21:05 -0300

    Exercise 1035 - Beginner

--- Beginner/1035.c
+++ Beginner/1035.c
@@ -1,2 +1,30 @@
-/*******************************
+/*
+ * URI Online Judge - https://www.urionlinejudge.com.br
+ * Beginner
+ * Problem 1021
  *
+ * Author: Gabriel Oliveira dos Santos
+ * 2016
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+	int a, b, c, d;
+	bool accept = true;
+
+	scanf("%d %d %d %d", &a, &b, &c, &d);
+
+	accept = ((b > c) && (d > a) &&
+						(c + d > a + b)    &&
+						(c > 0 && d > 0)   &&
+						(a%2 == 0));
+
+	if(accept)
+		printf("Valores aceitos\n");
+	else
+		printf("Valores nao aceitos\n");
+
+	return 0;
+}
