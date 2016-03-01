0f7ad5bc0d74167609ba9fe92206cbbc5bbb0eed
Gabriel Oliveira <oliveirasgah@gmail.com>
2016-03-01 16:14:15 -0300

    Exercise 1035 - Beginner

--- Beginner/1035.c
+++ Beginner/1035.c
@@ -11,20 +11,20 @@
 #include <stdbool.h>
 
 int main() {
-	int a, b, c, d;
-	bool accept = true;
+   int a, b, c, d;
+   bool accept = true;
 
-	scanf("%d %d %d %d", &a, &b, &c, &d);
+   scanf("%d %d %d %d", &a, &b, &c, &d);
 
-	accept = ((b > c) && (d > a) &&
-						(c + d > a + b)    &&
-						(c > 0 && d > 0)   &&
-						(a%2 == 0));
+   accept = ((b > c) && (d > a) &&
+         (c + d > a + b)    &&
+         (c > 0 && d > 0)   &&
+         (a%2 == 0));
 
-	if(accept)
-		printf("Valores aceitos\n");
-	else
-		printf("Valores nao aceitos\n");
+   if(accept)
+      printf("Valores aceitos\n");
+   else
+      printf("Valores nao aceitos\n");
 
-	return 0;
+   return 0;
 }
