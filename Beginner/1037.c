e9d3013b37fc7ff587361f9bd20552dc6db9c00f
Gabriel Oliveira <oliveirasgah@gmail.com>
2016-03-01 16:14:29 -0300

    Exercise 1037 - Beginner

--- Beginner/1037.c
+++ Beginner/1037.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 int main() {
-	float value;
+   float value;
 
-	scanf("%f", &value);
+   scanf("%f", &value);
 
-	if(value < 0.00f || value > 100.00f)
-		printf("Fora de intervalo\n");
-	else {
-		if(value >= 0.00f && value <= 25.00f)
-			printf("Intervalo [0,25]\n");
-		else if(value >= 25.00001f && value <= 50.00f)
-			printf("Intervalo (25,50]\n");
-		else if(value >= 50.00001f && value <= 75.00f)
-			printf("Intervalo (50,75]\n");
-		else
-			printf("Intervalo (75,100]\n");
-	}
+   if(value < 0.00f || value > 100.00f)
+      printf("Fora de intervalo\n");
+   else {
+      if(value >= 0.00f && value <= 25.00f)
+         printf("Intervalo [0,25]\n");
+      else if(value >= 25.00001f && value <= 50.00f)
+         printf("Intervalo (25,50]\n");
+      else if(value >= 50.00001f && value <= 75.00f)
+         printf("Intervalo (50,75]\n");
+      else
+         printf("Intervalo (75,100]\n");
+   }
 
-	return 0;
+   return 0;
 }
