3bb101b12b2bf7de0168110f050c205586fcd5ef
Gabriel Oliveira <oliveirasgah@gmail.com>
2016-03-01 16:15:14 -0300

    Exercise 1026 - Ad-hoc

--- Ad-hoc/1026.c
+++ Ad-hoc/1026.c
@@ -2,10 +2,10 @@
 #include <stdint.h>
 
 int main() {
-	uint32_t a, b;
+   uint32_t a, b;
 
-	while(scanf("%u %u", &a, &b) != EOF)
-		printf("%u\n", (a & (~b)) | ((~a) & b));
+   while(scanf("%u %u", &a, &b) != EOF)
+      printf("%u\n", (a & (~b)) | ((~a) & b));
 
-	return 0;
+   return 0;
 }
