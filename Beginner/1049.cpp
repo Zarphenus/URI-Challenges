60438750fdcbc427670449c83a9ae398bc9ce67c
Gabriel Oliveira <oliveirasgah@gmail.com>
2016-04-01 16:54:24 -0300

    Exercise 1049 - Beginner

---
+++ Beginner/1049.cpp
@@ -0,0 +1,48 @@
+/*
+ * URI Online Judge - https://www.urionlinejudge.com.br
+ * Beginner
+ * Problem 1049
+ *
+ * Author: Gabriel Oliveira dos Santos
+ * 2016
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   string a, b, c;
+
+   cin >> a >> b >> c;
+
+   if(a == "vertebrado") {
+      if(b == "ave") {
+         if(c == "carnivoro")
+            cout << "aguia" << endl;
+         else if(c == "onivoro")
+            cout << "pomba" << endl;
+      }
+      else if(b == "mamifero") {
+         if(c == "onivoro")
+            cout << "homem" << endl;
+         else if(c == "herbivoro")
+            cout << "vaca" << endl;
+      }
+   } else if(a == "invertebrado") {
+      if(b == "inseto") {
+         if(c == "hematofago")
+            cout << "pulga" << endl;
+         else if(c == "herbivoro")
+            cout << "lagarta" << endl;
+      }
+      else if(b == "anelideo") {
+         if(c == "hematofago")
+            cout << "sanguessuga" << endl;
+         else if(c == "onivoro")
+            cout << "minhoca" << endl;
+      }
+   }
+
+   return 0;
+}
