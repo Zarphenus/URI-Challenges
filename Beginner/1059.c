984062b965d94e45ba42be94ef360652ca591078
Gabriel Oliveira <oliveirasgah@gmail.com>
2016-04-05 14:05:38 -0300

    Exercise 1059 - Beginner

---
+++ Beginner/1059.c
@@ -0,0 +1,21 @@
+/*
+ * URI Online Judge - https://www.urionlinejudge.com.br
+ * Beginner
+ * Problem 1059
+ *
+ * Author: Gabriel Oliveira dos Santos
+ * 2016
+ */
+
+#include <stdio.h>
+
+int main() {
+   int num = 0;
+
+   while(num < 100) {
+      num += 2;
+      printf("%d\n", num);
+   }
+
+   return 0;
+}
