645284558d394a76259b634443d179739ff3fce6
Gabriel Oliveira <oliveirasgah@gmail.com>
2016-04-07 15:09:46 -0300

    Exercise 1115 - Beginner

---
+++ Beginner/1115.c
@@ -0,0 +1,35 @@
+/*
+ * URI Online Judge - https://www.urionlinejudge.com.br
+ * Beginner
+ * Problem 1115
+ *
+ * Author: Gabriel Oliveira dos Santos
+ * 2016
+ */
+
+#include <stdio.h>
+
+int main() {
+   int x, y;
+
+   scanf("%d %d", &x, &y);
+
+   while(x != 0 && y != 0) {
+      if(x > 0) {
+         if(y > 0)
+            printf("primeiro\n");
+         else
+            printf("quarto\n");
+      } else {
+         if(y > 0)
+            printf("segundo\n");
+         else
+            printf("terceiro\n");
+      }
+
+      
+      scanf("%d %d", &x, &y);
+   }
+
+   return 0;
+}
