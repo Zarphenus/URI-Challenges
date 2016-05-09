a5244f2c8f341aa738c5eb9e19ace2ee54b3abaf
Gabriel Oliveira <oliveirasgah@gmail.com>
2016-05-09 17:30:54 -0300

    Exercise 1149 - Beginner

---
+++ Beginner/1149.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+   int a, n, sum = 0;
+
+   scanf("%d %d", &a, &n);
+
+   while(n <= 0)
+      scanf("%d", &n);
+
+   while(n-- != 0)
+      sum += a++;
+
+   printf("%d\n", sum);
+
+   return 0;
+}
